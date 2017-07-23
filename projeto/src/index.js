8447b1e9adac9f430923ef9fbd342821782f43ec
João Victor <joaovpbarbosa@gmail.com>
2017-07-23 00:07:01 -0300

    deleting past project

--- projeto/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
-import 'spectre.css/dist/spectre.min.css'
-import 'spectre.css/dist/spectre-icons.min.css'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
